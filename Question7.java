989c2a311f88b607402428909e827ec60a3beb3e
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-07-17 22:57:08 +0530

    new commit

--- Question7.java
+++ Question7.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Question7 {
+    public static void main(String[] args) {
+        //reverse of a number
+        System.out.print("Enter a number to be reversed : ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int ans = 0;
+
+        while(num>0){
+            int rem = num%10;
+            ans = ans * 10 + rem;
+            num = num/10;//this was basically the missing step
+        }
+        System.out.println(ans);
+    }
 }
