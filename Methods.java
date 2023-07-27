236a132c3224cd2f73fe045d89f2518bc251aae4
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-07-28 00:40:23 +0530

    New Commits

--- Methods.java
+++ Methods.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Methods {
-    static void greet(){
-        System.out.println("Hi I am Kshitij Sharma");
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter first number :  ");
+        int a = in.nextInt();
+        System.out.println("Enter second number :  ");
+        int b = in.nextInt();
+        int sum = a + b ;
+        System.out.println("The sum of the numbers is: "+ sum);
+
     }
     public static void main(String[] args) {
+        sum();
 
 
 
