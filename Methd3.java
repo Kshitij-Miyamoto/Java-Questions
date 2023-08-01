1dbcaee49ee6d0f8480d6dc3f0a3d7f5c39c6654
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-08-01 18:50:02 +0530

    New Commits

---
+++ Methd3.java
@@ -0,0 +1,11 @@
+public class Methd3 {
+    static void info(String fname , int age ){
+        System.out.println(fname + "is " + age);
+    }
+//In this code we tend to pass multiple Parameters in the program
+    public static void main(String[] args) {
+        info("Kshitij " , 20);
+        info("Shouray " , 16);
+        info("Salu " , 18);
+    }
+}
---
+++ return1.java
@@ -0,0 +1,9 @@
+public class return1 {
+    static int sum(int x){
+        return (5 + x);
+    }
+    //here we make use of return values, and understand how can we use return values
+    public static void main(String[] args) {
+        System.out.println(sum(12));
+    }
+}
