b52477647db049751dc578a34e3d7396141f209b
Kshitij Sharma <110044782+Kshitij210@users.noreply.github.com>
2023-08-03 19:42:54 +0530

    New Commits

--- Recursion1.java
+++ Recursion1.java
@@ -1,2 +1,16 @@
 public class Recursion1 {
+    public static int sum(int start, int end){
+        //using recursion to print sum of numbers from 5 to 10
+        if(end>start){
+            return end + sum(start, end-1);
+        }
+        else{
+            return 0;
+        }
+    }
+//With this recursive program we can actually find any number digit sums
+    public static void main(String[] args) {
+        int x = sum(5,10);
+        System.out.println(x);
+    }
 }
